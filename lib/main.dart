f9c94cf6a8486b8f977541e5141cf9664b332113
Fábio Mendes <fabiobends@gmail.com>
2020-06-20 23:30:37 -0300

    drawer and avatar

--- lib/main.dart
+++ lib/main.dart
@@ -2,51 +2,53 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: HomePage(),
-    theme: ThemeData(
-      primarySwatch: Colors.blue,
-    )
-  ));
+      home: HomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      )));
 }
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('My Awesome App')
+      appBar: AppBar(title: Text('My Awesome App')),
+      body: Center(
+        child: Container(
+          height: 100,
+          width: 100,
+          color: Colors.black87,
+        ),
       ),
-      body: Container(
-        height: 100,
-        color: Colors.black87,
-        child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                padding: const EdgeInsets.all(10),
-                // color: Colors.red,
-                height: 100,
-                width: 100,
-                alignment: Alignment.center,
-                color: Colors.green
-              ),Container(
-                padding: const EdgeInsets.all(10),
-                // color: Colors.red,
-                height: 100,
-                width: 100,
-                alignment: Alignment.center,
-                color: Colors.white
-              ),Container(
-                padding: const EdgeInsets.all(10),
-                // color: Colors.red,
-                height: 100,
-                width: 100,
-                alignment: Alignment.center,
-                color: Colors.red
-              ),
-            ],
+      drawer: Drawer(
+          child: ListView(
+        padding: const EdgeInsets.all(0),
+        children: <Widget>[
+          UserAccountsDrawerHeader(
+              accountName: Text("Me"),
+              accountEmail: Text("A correct e-mail"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://images.unsplash.com/photo-1529218129894-37526611a113?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"),
+              )),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('Me'),
+            subtitle: Text('Developer'),
+            trailing: Icon(Icons.edit),
           ),
+          ListTile(
+            leading: Icon(Icons.email),
+            title: Text('Other'),
+            subtitle: Text("Doesn't matter"),
+            trailing: Icon(Icons.edit),
+            onTap: () {},
+          )
+        ],
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.edit),
       ),
     );
   }
